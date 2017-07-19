a01767fdbeb3e6e894371efb628b828aaad30108
Diasan Abria Diaz Jr <Diasan Abria Diaz Jr>
2017-07-19 20:34:32 +0800

    Add project files.

---
+++ OpenXml/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace OpenXml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbLot = new System.Windows.Forms.TextBox();
+            this.tbBlock = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbMonthlyPay = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(618, 552);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(593, 65);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // tbLot
+            // 
+            this.tbLot.Location = new System.Drawing.Point(593, 112);
+            this.tbLot.Name = "tbLot";
+            this.tbLot.Size = new System.Drawing.Size(100, 20);
+            this.tbLot.TabIndex = 3;
+            // 
+            // tbBlock
+            // 
+            this.tbBlock.Location = new System.Drawing.Point(593, 171);
+            this.tbBlock.Name = "tbBlock";
+            this.tbBlock.Size = new System.Drawing.Size(100, 20);
+            this.tbBlock.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(507, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(507, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Block #:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(507, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Lot #:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(510, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Monthly:";
+            // 
+            // tbMonthlyPay
+            // 
+            this.tbMonthlyPay.Location = new System.Drawing.Point(593, 224);
+            this.tbMonthlyPay.Name = "tbMonthlyPay";
+            this.tbMonthlyPay.Size = new System.Drawing.Size(100, 20);
+            this.tbMonthlyPay.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column5,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 310);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(786, 201);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Ok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Payment";
+            this.Column5.Name = "Column5";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Lot #";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Block #";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Monthly Pay";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 587);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbMonthlyPay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbBlock);
+            this.Controls.Add(this.tbLot);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbLot;
+        private System.Windows.Forms.TextBox tbBlock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbMonthlyPay;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ OpenXml/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace OpenXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "C:\\Users\\Mew\\Documents\\Visual Studio 2017\\Projects\\OpenXml\\TestFilesExcel";
+            openFileDialog1.Filter = "files (*.xlsx)|*.xlsx";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            string fileName = openFileDialog1.FileName;
+                            string today = null;
+                            //@"C:\users\Mew\Downloads\CEASARIO CONCHITA.xlsx";
+                            //DateTime test2 = DateTime.UtcNow.Date;
+                            // Retrieve the value in cell A1.
+                            string value = GetCellValue(fileName, "Sheet1", "A7");
+                            string lotNumberValue = GetCellValue(fileName, "Sheet1", "B8");
+                            string blockNumberValue = GetCellValue(fileName, "Sheet1", "B9");
+                            
+                           
+                            MessageBox.Show("Name: " + value +
+                                            "\nLot: " + lotNumberValue +
+                                            "\nBlock: " + blockNumberValue +
+                                            "\nDate Computed: " + GetDateValue(today));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private string GetDateValue(string today)
+        {
+            DateTime today1 = DateTime.UtcNow.Date;
+            
+            return today1.ToString("MMMM dd, yyyy");
+        }
+
+      
+
+        public static string GetCellValue(string fileName,
+            string sheetName,
+            string addressName)
+        {
+            string value = null;
+
+            // Open the spreadsheet document for read-only access.
+            using (SpreadsheetDocument document =
+                SpreadsheetDocument.Open(fileName, false))
+            {
+                // Retrieve a reference to the workbook part.
+                WorkbookPart wbPart = document.WorkbookPart;
+
+                // Find the sheet with the supplied name, and then use that 
+                // Sheet object to retrieve a reference to the first worksheet.
+                Sheet theSheet = wbPart.Workbook.Descendants<Sheet>().
+                  Where(s => s.Name == sheetName).FirstOrDefault();
+
+                // Throw an exception if there is no sheet.
+                if (theSheet == null)
+                {
+                    throw new ArgumentException("sheetName");
+                }
+
+                // Retrieve a reference to the worksheet part.
+                WorksheetPart wsPart =
+                    (WorksheetPart)(wbPart.GetPartById(theSheet.Id));
+
+                // Use its Worksheet property to get a reference to the cell 
+                // whose address matches the address you supplied.
+                Cell theCell = wsPart.Worksheet.Descendants<Cell>().
+                  Where(c => c.CellReference == addressName).FirstOrDefault();
+
+                // If the cell does not exist, return an empty string.
+                if (theCell != null)
+                {
+                    value = theCell.InnerText;
+
+                    // If the cell represents an integer number, you are done. 
+                    // For dates, this code returns the serialized value that 
+                    // represents the date. The code handles strings and 
+                    // Booleans individually. For shared strings, the code 
+                    // looks up the corresponding value in the shared string 
+                    // table. For Booleans, the code converts the value into 
+                    // the words TRUE or FALSE.
+                    if (theCell.DataType != null)
+                    {
+                        switch (theCell.DataType.Value)
+                        {
+                            case CellValues.SharedString:
+
+                                // For shared strings, look up the value in the
+                                // shared strings table.
+                                var stringTable =
+                                    wbPart.GetPartsOfType<SharedStringTablePart>()
+                                    .FirstOrDefault();
+
+                                // If the shared string table is missing, something 
+                                // is wrong. Return the index that is in
+                                // the cell. Otherwise, look up the correct text in 
+                                // the table.
+                                if (stringTable != null)
+                                {
+                                    value =
+                                        stringTable.SharedStringTable
+                                        .ElementAt(int.Parse(value)).InnerText;
+                                }
+                                break;
+
+                            case CellValues.Boolean:
+                                switch (value)
+                                {
+                                    case "0":
+                                        value = "FALSE";
+                                        break;
+                                    default:
+                                        value = "TRUE";
+                                        break;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return value;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
